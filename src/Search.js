0eb4ae107f80a01d37e7131a070b29dba75958b5
Samantha Aleman <samantha.aleman25@gmail.com>
2021-07-05 15:42:08 -0700

    starting to add the search functionality

--- src/Search.js
+++ src/Search.js
@@ -3,28 +3,49 @@ import './Search.css';
 import SearchIcon from '@material-ui/icons/Search';
 import MicIcon from '@material-ui/icons/Mic';
 import { Button } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 
-function Search() {
+function Search({ hideButtons = false }) {
 
     const [input, setInput] = useState('');
+    const history = useHistory();
 
     const search = e => {
         e.preventDefault();
+
+        console.log("You inputted: ", input)
+
+        history.push('/search')
     };
 
 
     return (
-        <div className='search'>
+        <form className='search'>
+            
             <div className='search_input'>
                 <SearchIcon className='search_inputIcon' />
                 <input value={input} onChange={e => setInput(e.target.value)} />
                 <MicIcon />
             </div>
+
+            {!hideButtons ? (
+            
             <div className='search_buttons'>
-                <Button onClick={search} variant='outlined'>Google Search</Button>
+                <Button type='submit' onClick={search} variant='outlined'>Google Search</Button>
                 <Button variant='outlined'>I'm Feeling Lucky</Button>
             </div>
-        </div>
+            
+            ): (
+                
+            <div className='search_buttons'>
+                <Button className='search_buttonsHidden' type='submit' onClick={search} variant='outlined'>Google Search</Button>
+                <Button className='search_buttonsHidden' variant='outlined'>I'm Feeling Lucky</Button>
+            </div>
+            
+            )}
+
+            
+        </form>
     )
 }
 
