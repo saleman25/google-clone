083643ddfbae0fd6050a7a29ef802f09f0ad4302
Samantha Aleman <samantha.aleman25@gmail.com>
2021-07-05 14:18:51 -0700

    added material-ui started home component

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>hi :3</h1>
+
+      {/*Home (the one with the search on*/}
+        <Home />
+
+      {/*SearchPage (the results page) */}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <div className='home'>
+            <h1>this homepage</h1>
+
+            <div className='home_header'>
+                <div className='home_headerLeft'>
+                    {/* Link */}
+                     {/* Link */}
+                </div>
+
+                <div className='home_headerRight'>
+                     {/* Link */}
+                      {/* Link */}
+                       {/* Icon */}
+                       {/* Avatar */}
+                       
+                </div>
+            </div>
+
+            <div className='home_body'>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Home
