3c88fb08e32371eb006b296fe25cd3d1972bc29b
Samantha Aleman <samantha.aleman25@gmail.com>
2021-07-05 19:07:03 -0700

    populating search page results with retrieved data from api

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -2,83 +2,101 @@ import React from "react";
 import "./SearchPage.css";
 import { useStateValue } from "./StateProvider";
 import useGoogleSearch from "./useGoogleSearch";
-import { Link } from 'react-router-dom';
-import Search from './Search';
-import SearchIcon from '@material-ui/icons/Search';
-import DescriptionIcon from '@material-ui/icons/Description';
-import ImageIcon from '@material-ui/icons/Image';
-import LocalOfferIcon from '@material-ui/icons/LocalOffer';
-import RoomIcon from '@material-ui/icons/Room';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { Link } from "react-router-dom";
+import Search from "./Search";
+import SearchIcon from "@material-ui/icons/Search";
+import DescriptionIcon from "@material-ui/icons/Description";
+import ImageIcon from "@material-ui/icons/Image";
+import LocalOfferIcon from "@material-ui/icons/LocalOffer";
+import RoomIcon from "@material-ui/icons/Room";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 // import Response from './response'
 
 function SearchPage() {
   const [{ term }, dispatch] = useStateValue();
   const { data } = useGoogleSearch(term);
 
-//   const data = Response;
+  //   const data = Response;
   console.log(data);
 
   return (
     <div className="searchPage">
       <div className="searchPage_header">
-        <Link to='/'>
-        <img 
+        <Link to="/">
+          <img
             className="searchPage_logo"
-            src='https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'
-            alt=''
-            />
+            src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+            alt=""
+          />
         </Link>
 
-        <div className='searchPage_headerBody'>
-           
-            <Search hideButtons />
-            
-            <div className='searchPage_options'>
-                
-                <div className="searchPage_optionsLeft">
-                    <div className="searchPage_option">
-                    <SearchIcon />
-                    <Link to='/all'>All</Link>
-                    </div>
-                    <div className="searchPage_option">
-                    <DescriptionIcon />
-                    <Link to='/news'>News</Link>
-                    </div>
-                    <div className="searchPage_option">
-                    <ImageIcon />
-                    <Link to='/images'>Images</Link>
-                    </div>
-                    <div className="searchPage_option">
-                    <LocalOfferIcon />
-                    <Link to='/shopping'>Shopping</Link>
-                    </div>
-                    <div className="searchPage_option">
-                    <RoomIcon />
-                    <Link to='/maps'>Maps</Link>
-                    </div>
-                    <div className="searchPage_option">
-                    <MoreVertIcon />
-                    <Link to='/more'>More</Link>
-                    </div>
-                </div>
-                
-                <div className="searchPage_optionsRight">
-                    <div className="searchPage_option">
-                    <Link to='/settings'>Settings</Link>
-                    </div>
-                    <div className="searchPage_option">
-                    <Link to='/tools'>Tools</Link>
-                    </div>
-                </div>
+        <div className="searchPage_headerBody">
+          <Search hideButtons />
 
+          <div className="searchPage_options">
+            <div className="searchPage_optionsLeft">
+              <div className="searchPage_option">
+                <SearchIcon />
+                <Link to="/all">All</Link>
+              </div>
+              <div className="searchPage_option">
+                <DescriptionIcon />
+                <Link to="/news">News</Link>
+              </div>
+              <div className="searchPage_option">
+                <ImageIcon />
+                <Link to="/images">Images</Link>
+              </div>
+              <div className="searchPage_option">
+                <LocalOfferIcon />
+                <Link to="/shopping">Shopping</Link>
+              </div>
+              <div className="searchPage_option">
+                <RoomIcon />
+                <Link to="/maps">Maps</Link>
+              </div>
+              <div className="searchPage_option">
+                <MoreVertIcon />
+                <Link to="/more">More</Link>
+              </div>
             </div>
 
+            <div className="searchPage_optionsRight">
+              <div className="searchPage_option">
+                <Link to="/settings">Settings</Link>
+              </div>
+              <div className="searchPage_option">
+                <Link to="/tools">Tools</Link>
+              </div>
+            </div>
+          </div>
         </div>
-
       </div>
 
-      <div className="searchPage_results"></div>
+        {term && ( 
+            <div className="searchPage_results">
+                <p className="searchPage_resultCount">
+                    About {data?.searchInformation.formattedTotalResults} results 
+                    ({data?.searchInformation.formattedSearchTime} seconds) for {term}.
+                </p> 
+
+                {data?.items.map(item => (
+                    <div className='searchPage-result'>
+                        <a href={item.link}>
+                            {item.displayLink} ▽
+                        </a>
+                        <a className="searchPage_resultTitle" href={item.link}>
+                            <h2>{item.title}</h2>
+                        </a>
+                        <p className="searchPage_resultSnippet">
+                            {item.snippet}
+                        </p>
+                    </div>
+                ))}
+
+            </div>
+          )}
+      
     </div>
   );
 }
